a0ec345bfe89b53ef5fea9914201024f32f43053
Alen Joby <alenjoby955@gmail.com>
2026-04-18 02:39:44 +0400

    chore: wipe repository

--- js/env-init.js
+++
@@ -1,6 +0,0 @@
-import { API_URL } from "./env.js";
-
-// Bridge for legacy / non-module scripts (like studio.js, dashboard.js, etc.)
-window.__INVIO_API_URL__ = API_URL;
-
-console.log("[INVIO Shared] API URL initialized globally:", window.__INVIO_API_URL__);
